c33c6c44ae5d42eb59669920e467afd4ea5bcbde
John McLear <john@mclear.co.uk>
2013-01-18 22:46:09 +0000

    fix express path

--- hooks.js
+++ hooks.js
@@ -1,6 +1,6 @@
-var path = require('path');
-var express = require('express');
-var eejs = require("ep_etherpad-lite/node/eejs");
+var path = require('path'), 
+    express = require('ep_etherpad-lite/node_modules/express'),
+    eejs = require("ep_etherpad-lite/node/eejs");
 
 exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
   args.content = args.content + eejs.require("ep_embedmedia/templates/editbarButtons.ejs", {}, module);
